142261d7d7451c9b0172c02a5d7e8bce0d196be5
vilcu.alexandru <>
2025-09-25 18:09:42 +0200

    first commit

---
+++ src/main/java/com/panda/security/book/BookService.java
@@ -0,0 +1,28 @@
+package com.panda.security.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    public void save(BookRequest request) {
+        var book = Book.builder()
+                .id(request.getId())
+                .author(request.getAuthor())
+                .isbn(request.getIsbn())
+                .build();
+        repository.save(book);
+    }
+
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+}
